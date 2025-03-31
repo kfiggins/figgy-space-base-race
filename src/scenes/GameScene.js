3683c98defbf3094538dfecbc0c69414f81a707e
Carter Figgins <Carter.Figgins@atomicjolt.com>
2025-03-30 22:15:57 -0600

    added velocity

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -13,6 +13,10 @@ class GameScene extends Phaser.Scene {
     this.spaceship = this.add.image(400, 300, "spaceship");
     this.spaceship.setScale(0.2);
     this.spaceship.setDepth(1);
+    this.spaceship.speed = 0;
+    this.spaceship.velocityX = 0;
+    this.spaceship.velocityY = 0;
+    this.spaceship.maxSpeed = 0;
 
     this.particleEmitter = this.add.particles(0, 0, "spark", {
       x: { min: -5, max: 5 },
@@ -37,25 +41,28 @@ class GameScene extends Phaser.Scene {
   }
 
   update() {
-    const { wasd, spaceship, particleEmitter } = this;
-
-    let velocityX = 0;
-    let velocityY = 0;
-    let isMoving = false;
+    const { wasd, spaceship, particleEmitter, sys } = this;
+    const { width: gameWidth, height: gameHight } = sys.game.canvas;
+    const speedChange = 0.05
+    const maxSpeed = 5;
+    const friction = 1.03;
+    const minVelocity = 0.1;
+    let yIsMoving = false;
+    let xIsMoving = false;
 
     if (wasd.left.isDown) {
-      velocityX = -5;
+      spaceship.velocityX += -speedChange;
       spaceship.rotation = Math.PI * 1.5;
-      isMoving = true;
+      xIsMoving = true;
     } else if (wasd.right.isDown) {
-      velocityX = 5;
+      spaceship.velocityX += speedChange;
       spaceship.rotation = Math.PI / 2;
-      isMoving = true;
+      xIsMoving = true;
     }
 
     if (wasd.up.isDown) {
-      velocityY = -5;
-      isMoving = true;
+      spaceship.velocityY += -speedChange;
+      yIsMoving = true;
       spaceship.rotation = 0;
       if (wasd.left.isDown) {
         spaceship.rotation = Math.PI * 1.7;
@@ -65,8 +72,8 @@ class GameScene extends Phaser.Scene {
         spaceship.rotation = 0;
       }
     } else if (wasd.down.isDown) {
-      velocityY = 5;
-      isMoving = true;
+      spaceship.velocityY += speedChange;
+      yIsMoving = true;
       if (wasd.left.isDown) {
         spaceship.rotation = Math.PI * 1.25;
       } else if (wasd.right.isDown) {
@@ -76,10 +83,38 @@ class GameScene extends Phaser.Scene {
       }
     }
 
-    spaceship.x += velocityX;
-    spaceship.y += velocityY;
+    // hit max speed. remove for fun
+    if (spaceship.velocityX > maxSpeed) {
+      spaceship.velocityX = maxSpeed
+    }
+    if (spaceship.velocityX < -maxSpeed) {
+      spaceship.velocityX = -maxSpeed
+    }
+    if (spaceship.velocityY > maxSpeed) {
+      spaceship.velocityY = maxSpeed
+    }
+    if (spaceship.velocityY < -maxSpeed) {
+      spaceship.velocityY = -maxSpeed
+    }
 
-    if (isMoving) {
+    spaceship.x += spaceship.velocityX;
+    spaceship.y += spaceship.velocityY;
+
+    // Can remove when we have a way to move through map
+    if (spaceship.x > gameWidth) {
+      spaceship.x = 0
+    }
+    if (spaceship.x < 0) {
+      spaceship.x = gameWidth
+    }
+    if (spaceship.y > gameHight) {
+      spaceship.y = 0
+    }
+    if (spaceship.y < 0) {
+      spaceship.y = gameHight
+    }
+
+    if (yIsMoving || xIsMoving) {
       particleEmitter.ops.angle.loadConfig({
         angle: {
           min: ((spaceship.rotation + Math.PI / 2) * 180) / Math.PI - 15,
@@ -87,6 +122,20 @@ class GameScene extends Phaser.Scene {
         },
       });
       particleEmitter.emitParticle(2);
+    }
+
+    // Stopping the ship with some cool friction
+    if (!yIsMoving && spaceship.velocityY !== 0) {
+      spaceship.velocityY /= friction;
+      if (Math.abs(spaceship.velocityY) < minVelocity) {
+        spaceship.velocityY = 0;
+      }
+    }
+    if (!xIsMoving && spaceship.velocityX !== 0) {
+      spaceship.velocityX /= friction;
+      if (Math.abs(spaceship.velocityX) < minVelocity) {
+        spaceship.velocityX = 0;
+      }
     }
   }
 }
